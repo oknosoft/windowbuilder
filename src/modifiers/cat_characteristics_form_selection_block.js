f2bd1ee7ccf4cb43d3df30e011424d4e42168d1e
unpete <info@oknosoft.ru>
2016-03-25 16:17:36 +0500

    v0.1.3

--- src/modifiers/cat_characteristics_form_selection_block.js
+++ src/modifiers/cat_characteristics_form_selection_block.js
@@ -11,7 +11,8 @@ $p.modifiers.push(
 	function($p) {
 
 		var _mgr = $p.cat.characteristics,
-			selection_block;
+			_meta = $p.cat.characteristics.metadata()._clone(),
+			selection_block, wnd;
 
 		// попробуем подсунуть типовой форме выбора виртуальные метаданные - с деревом и ограниченным списком значений
 		_mgr.form_selection_block = function(pwnd, attr){
@@ -20,51 +21,54 @@ $p.modifiers.push(
 				attr = {};
 
 			if(!selection_block){
+
 				selection_block = {
 					_obj: {
 						_calc_order: $p.blank.guid
 					}
 				};
 
+				_meta.form = {
+					selection: {
+						fields: ["name as presentation","s"],
+						cols: [
+							{"id": "presentation", "width": "260", "type": "ro", "align": "left", "sort": "na", "caption": "Наименование"},
+							{"id": "svg", "width": "*", "type": "rsvg", "align": "left", "sort": "na", "caption": "Эскиз"}
+						]
+					}
+				};
+
 				selection_block.__define({
 
-					// виртуальные метаданные для автоформ
+					// виртуальные метаданные для поля фильтра по заказу
 					_metadata: {
 						get : function(){
-							var t = this,
-								calc_order = $p.cat.characteristics.metadata().fields.calc_order._clone();
 
-							calc_order.choice_links = [{
-								name: ["selection",	"ref"],
-								path: [
-									function(o, f){
-										var selection = t instanceof Filling ?
-										{elm_type: {in: [$p.enm.elm_types.Стекло, $p.enm.elm_types.Заполнение]}} :
-										{elm_type: t.nom.elm_type};
-
-										if($p.is_data_obj(o)){
-											var ok = false;
-											selection.nom = o;
-											t.project._dp.sys.elmnts.find_rows(selection, function (row) {
-												ok = true;
-												return false;
-											});
-											return ok;
-										}else{
-											var refs = "";
-											t.project._dp.sys.elmnts.find_rows(selection, function (row) {
-												if(refs)
-													refs += ", ";
-												refs += "'" + row.nom.ref + "'";
-											});
-											return "_t_.ref in (" + refs + ")";
-										}
-									}]}
-							];
+							// возвращаем типовые метаданные зарактеристики, но могли бы вернуть изменённый клон
+							//var calc_order = $p.cat.characteristics.metadata().fields.calc_order._clone();
+							// calc_order.choice_links = [{
+							// 	name: ["selection",	"s"],
+							// 	path: [
+							// 		function(o, f){
+							//
+							// 			if($p.is_data_obj(o)){
+							// 				return o.s > 0;
+							//
+							// 			}else{
+							// 				var refs = "";
+							// 				t.project._dp.sys.elmnts.find_rows(selection, function (row) {
+							// 					if(refs)
+							// 						refs += ", ";
+							// 					refs += "'" + row.nom.ref + "'";
+							// 				});
+							// 				return "_t_.ref in (" + refs + ")";
+							// 			}
+							// 		}]}
+							// ];
 
 							return {
 								fields: {
-									calc_order: calc_order
+									calc_order: _meta.fields.calc_order
 								}
 							};
 						}
@@ -87,14 +91,16 @@ $p.modifiers.push(
 						set: function (v) {
 							if(this._obj[f] == v)
 								return;
-							_mgr._obj_сonstructor.prototype.__notify.call(this, "calc_order");
 							_mgr._obj_сonstructor.prototype.__setter.call(this, "calc_order", v);
+							if(wnd && wnd.elmnts && wnd.elmnts.filter)
+								wnd.elmnts.filter.call_event();
 						}
 					}
 
 				});
 			}
 
+			// объект отбора по ссылке на расчет в продукции
 			selection_block.calc_order = $p.wsql.get_user_param("template_block_calc_order");
 			if(!selection_block.calc_order.empty()){
 				$p.cat.predefined_elmnts.predefined("Расчет_ТиповойБлок").some(function (o) {
@@ -103,18 +109,21 @@ $p.modifiers.push(
 					return true;
 				});
 			}
+
+			// начальное значение - выбранные в предыдущий раз типовой блок
 			attr.initial_value = $p.wsql.get_user_param("template_block_initial_value");
-			if(!attr.initial_value){
 
-			}
-			//attr.selection = [{calc_order: selection_block.calc_order}];
+			// подсовываем типовой форме списка изменённые метаданные
+			attr.metadata = _meta;
 
-			var wnd = this.constructor.prototype.form_selection.call(this, pwnd, attr);
+			// создаём форму списка
+			wnd = this.constructor.prototype.form_selection.call(this, pwnd, attr);
 
 			wnd.elmnts.toolbar.hideItem("btn_new");
 			wnd.elmnts.toolbar.hideItem("btn_edit");
 			wnd.elmnts.toolbar.hideItem("btn_delete");
 
+			// добавляем элемент управления фильтра по расчету
 			wnd.elmnts.filter.add_filter({
 				text: "Расчет",
 				name: "calc_order"
@@ -139,6 +148,9 @@ $p.modifiers.push(
 				}
 			});
 			wnd.elmnts.filter.custom_selection.calc_order.getBase().style.border = "none";
+			
+			// добавляем пользовательский отбор по "площадь > 0"
+			wnd.elmnts.filter.custom_selection.s = {value: {gt: 0}};
 
 			return wnd;
 		};
