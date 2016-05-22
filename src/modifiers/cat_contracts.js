cffc0d0eccd951c98d8f124067db325ebfbaf028
unpete <info@oknosoft.ru>
2016-05-22 21:12:48 +0500

    v0.1.3

--- src/modifiers/cat_contracts.js
+++ src/modifiers/cat_contracts.js
@@ -13,9 +13,10 @@ $p.modifiers.push(
 		var _mgr = $p.cat.contracts;
 
 		_mgr.sql_selection_list_flds = function(initial_value){
-			return "SELECT _t_.ref, _t_.`_deleted`, _t_.is_folder, _t_.id, _t_.name as presentation, _k_.synonym as contract_kind, _m_.synonym as mutual_settlements, _o_.name as organization," +
+			return "SELECT _t_.ref, _t_.`_deleted`, _t_.is_folder, _t_.id, _t_.name as presentation, _k_.synonym as contract_kind, _m_.synonym as mutual_settlements, _o_.name as organization, _p_.name as partner," +
 				" case when _t_.ref = '" + initial_value + "' then 0 else 1 end as is_initial_value FROM cat_contracts AS _t_" +
 				" left outer join cat_organizations as _o_ on _o_.ref = _t_.organization" +
+				" left outer join cat_partners as _p_ on _p_.ref = _t_.owner" +
 				" left outer join enm_mutual_contract_settlements as _m_ on _m_.ref = _t_.mutual_settlements" +
 				" left outer join enm_contract_kinds as _k_ on _k_.ref = _t_.contract_kind %3 %4 LIMIT 300";
 		};
