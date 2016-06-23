b0e4ea9fda077daa52fcb57403c09ff5b52f76e3
unpete <info@oknosoft.ru>
2016-06-23 22:20:29 +0500

    v0.1.3

--- dist/wnd_debug.js
+++ dist/wnd_debug.js
@@ -1470,6 +1470,61 @@ $p.modifiers.push(
 $p.modifiers.push(
 	function($p) {
 
+		$p.cat.users_acl.__define({
+
+			load_array: {
+				value: function(aattr, forse){
+
+					var ref, obj, res = [], acl;
+
+					for(var i in aattr){
+
+						ref = $p.fix_guid(aattr[i]);
+
+						acl = aattr[i].acl;
+						if(acl)
+							delete aattr[i].acl;
+
+						if(!(obj = this.by_ref[ref])){
+							obj = new this._obj_constructor(aattr[i], this);
+							if(forse)
+								obj._set_loaded();
+
+						}else if(obj.is_new() || forse){
+							obj._mixin(aattr[i]);
+							obj._set_loaded();
+						}
+
+						if(acl && !obj._acl){
+							var _acl = {};
+							for(var i in acl){
+								_acl.__define(i, {
+									value: {},
+									writable: false
+								});
+								for(var j in acl[i]){
+									_acl[i].__define(j, {
+										value: acl[i][j],
+										writable: false
+									});
+								}
+							}
+							obj.__define({
+								_acl: {
+									value: _acl,
+									writable: false
+								}
+							});
+						}
+
+						res.push(obj);
+					}
+
+					return res;
+				}
+			}
+		});
+
 		$p.cat.users_acl._obj_constructor.prototype.__define({
 
 			role_available: {
