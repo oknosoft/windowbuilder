d40992c4d20e89e3cf8981211fcb28eee7221da3
unpete <info@oknosoft.ru>
2017-02-19 01:54:22 +0500

    размерные линии - отладка, слой соединителей

--- src/geometry/dimension_line.js
+++ src/geometry/dimension_line.js
@@ -21,383 +21,354 @@
  * @menuorder 46
  * @tooltip Размерная линия
  */
-function DimensionLine(attr){
+
+class DimensionLine extends paper.Group {
+
+  constructor(attr) {
+
+    super(attr);
+
+    this._row = attr.row;
+
+    if(this._row && this._row.path_data){
+      attr._mixin(JSON.parse(this._row.path_data));
+      if(attr.elm1){
+        attr.elm1 = this.project.getItem({elm: attr.elm1});
+      }
+      if(attr.elm2){
+        attr.elm2 = this.project.getItem({elm: attr.elm2});
+      }
+    }
+
+    this.data.pos = attr.pos;
+    this.data.elm1 = attr.elm1;
+    this.data.elm2 = attr.elm2 || this.data.elm1;
+    this.data.p1 = attr.p1 || "b";
+    this.data.p2 = attr.p2 || "e";
+    this.data.offset = attr.offset;
+
+    if(attr.impost){
+      this.data.impost = true;
+    }
+
+    if(attr.contour){
+      this.data.contour = true;
+    }
+
+    if(!this.data.pos && (!this.data.elm1 || !this.data.elm2)){
+      this.remove();
+      return null;
+    }
+
+    // создаём детей
+    new paper.Path({parent: this, name: 'callout1', strokeColor: 'black', guide: true});
+    new paper.Path({parent: this, name: 'callout2', strokeColor: 'black', guide: true});
+    new paper.Path({parent: this, name: 'scale', strokeColor: 'black', guide: true});
+    new paper.PointText({
+      parent: this,
+      name: 'text',
+      justification: 'center',
+      fillColor: 'black',
+      fontSize: 72});
+
+    this.on({
+      mouseenter: this._mouseenter,
+      mouseleave: this._mouseleave,
+      click: this._click
+    });
+
+    $p.eve.attachEvent("sizes_wnd", this._sizes_wnd.bind(this));
+
+  }
+
+  // виртуальные метаданные для автоформ
+  get _metadata() {
+    return $p.dp.builder_text.metadata();
+  }
+
+  // виртуальный датаменеджер для автоформ
+  get _manager() {
+    return $p.dp.builder_text;
+  }
+
+  _mouseenter() {
+    paper.canvas_cursor('cursor-arrow-ruler');
+  }
+
+  _mouseleave() {
+    //paper.canvas_cursor('cursor-arrow-white');
+  }
+
+  _click(event) {
+    event.stop();
+    this.wnd = new RulerWnd(null, this);
+    this.wnd.size = this.size;
+  }
+
+  _move_points(event, xy) {
+
+    let _bounds, delta;
+
+    // получаем дельту - на сколько смещать
+    if(this.data.elm1){
+
+      // в _bounds[event.name] надо поместить координату по x или у (в зависисмости от xy), которую будем двигать
+      _bounds = {};
+
+      if(this.pos == "top" || this.pos == "bottom"){
+
+        const size = Math.abs(this.data.elm1[this.data.p1].x - this.data.elm2[this.data.p2].x);
+
+        if(event.name == "right"){
+          delta = new paper.Point(event.size - size, 0);
+          _bounds[event.name] = Math.max(this.data.elm1[this.data.p1].x, this.data.elm2[this.data.p2].x);
+
+        }else{
+          delta = new paper.Point(size - event.size, 0);
+          _bounds[event.name] = Math.min(this.data.elm1[this.data.p1].x, this.data.elm2[this.data.p2].x);
+        }
 
 
-	DimensionLine.superclass.constructor.call(this, {parent: attr.parent});
+      }else{
 
-	var _row = attr.row;
+        const size = Math.abs(this.data.elm1[this.data.p1].y - this.data.elm2[this.data.p2].y);
 
-	if(_row && _row.path_data){
-		attr._mixin(JSON.parse(_row.path_data));
-		if(attr.elm1)
-			attr.elm1 = this.project.getItem({elm: attr.elm1});
-		if(attr.elm2)
-			attr.elm2 = this.project.getItem({elm: attr.elm2});
-	}
+        if(event.name == "bottom"){
+          delta = new paper.Point(0, event.size - size);
+          _bounds[event.name] = Math.max(this.data.elm1[this.data.p1].y, this.data.elm2[this.data.p2].y);
 
-	this.data.pos = attr.pos;
-	this.data.elm1 = attr.elm1;
-	this.data.elm2 = attr.elm2 || this.data.elm1;
-	this.data.p1 = attr.p1 || "b";
-	this.data.p2 = attr.p2 || "e";
-	this.data.offset = attr.offset;
+        }
+        else{
+          delta = new paper.Point(0, size - event.size);
+          _bounds[event.name] = Math.min(this.data.elm1[this.data.p1].y, this.data.elm2[this.data.p2].y);
+        }
+      }
 
-	if(attr.impost)
-		this.data.impost = true;
+    }else {
 
-	if(attr.contour)
-		this.data.contour = true;
+      _bounds = this.layer.bounds;
 
-	this.__define({
+      if(this.pos == "top" || this.pos == "bottom")
+        if(event.name == "right")
+          delta = new paper.Point(event.size - _bounds.width, 0);
+        else
+          delta = new paper.Point(_bounds.width - event.size, 0);
+      else{
+        if(event.name == "bottom")
+          delta = new paper.Point(0, event.size - _bounds.height);
+        else
+          delta = new paper.Point(0, _bounds.height - event.size);
+      }
 
-		_row: {
-			get: function () {
-				return _row;
-			}
-		},
+    }
 
-		/**
-		 * Удаляет элемент из контура и иерархии проекта
-		 * Одновлеменно, удаляет строку из табчасти табчасти _Координаты_
-		 * @method remove
-		 */
-		remove: {
-			value: function () {
-				if(_row){
-					_row._owner.del(_row);
-					_row = null;
-					this.project.register_change();
-				}
-				DimensionLine.superclass.remove.call(this);
-			}
-		}
-	});
+    if(delta.length){
 
-	if(!this.data.pos && (!this.data.elm1 || !this.data.elm2)){
-		this.remove();
-		return null;
-	}
+      paper.project.deselect_all_points();
 
-	// создаём детей
-	new paper.Path({parent: this, name: 'callout1', strokeColor: 'black', guide: true});
-	new paper.Path({parent: this, name: 'callout2', strokeColor: 'black', guide: true});
-	new paper.Path({parent: this, name: 'scale', strokeColor: 'black', guide: true});
-	new paper.PointText({
-		parent: this,
-		name: 'text',
-		justification: 'center',
-		fillColor: 'black',
-		fontSize: 72});
+      paper.project.getItems({class: Profile}).forEach(function (p) {
+        if(Math.abs(p.b[xy] - _bounds[event.name]) < consts.sticking0 && Math.abs(p.e[xy] - _bounds[event.name]) < consts.sticking0){
+          p.generatrix.segments.forEach(function (segm) {
+            segm.selected = true;
+          })
 
+        }else if(Math.abs(p.b[xy] - _bounds[event.name]) < consts.sticking0){
+          p.generatrix.firstSegment.selected = true;
 
-	this.on({
-		mouseenter: this._mouseenter,
-		mouseleave: this._mouseleave,
-		click: this._click
-	});
+        }else if(Math.abs(p.e[xy] - _bounds[event.name]) < consts.sticking0){
+          p.generatrix.lastSegment.selected = true;
 
-	$p.eve.attachEvent("sizes_wnd", this._sizes_wnd.bind(this));
+        }
 
+      });
+      this.project.move_points(delta);
+      setTimeout(function () {
+        this.deselect_all_points(true);
+        this.register_update();
+        //this.zoom_fit();
+      }.bind(this.project), 200);
+    }
+
+  }
+
+  _sizes_wnd(event) {
+
+    if(this.wnd && event.wnd == this.wnd.wnd){
+
+      switch(event.name) {
+        case 'close':
+          if(this.children.text){
+            this.children.text.selected = false;
+          }
+          this.wnd = null;
+          break;
+
+        case 'left':
+        case 'right':
+          if(this.pos == "top" || this.pos == "bottom"){
+            this._move_points(event, "x");
+          }
+          break;
+
+        case 'top':
+        case 'bottom':
+          if(this.pos == "left" || this.pos == "right"){
+            this._move_points(event, "y");
+          }
+          break;
+      }
+    }
+
+  }
+
+  redraw() {
+
+    const {layer, project, children, data, pos} = this;
+    if(!children.length){
+      return;
+    }
+    const _bounds = layer.bounds;
+    const _dim_bounds = layer instanceof DimensionLayer ? project.dimension_bounds : layer.dimension_bounds;
+    let offset = 0, b, e, bs, es;
+
+    if(!pos){
+      b = typeof data.p1 == "number" ? data.elm1.corns(data.p1) : data.elm1[data.p1];
+      e = typeof data.p2 == "number" ? data.elm2.corns(data.p2) : data.elm2[data.p2];
+    }
+    else if(pos == "top"){
+      b = _bounds.topLeft;
+      e = _bounds.topRight;
+      offset = _bounds[pos] - _dim_bounds[pos];
+    }
+    else if(pos == "left"){
+      b = _bounds.bottomLeft;
+      e = _bounds.topLeft;
+      offset = _bounds[pos] - _dim_bounds[pos];
+    }
+    else if(pos == "bottom"){
+      b = _bounds.bottomLeft;
+      e = _bounds.bottomRight;
+      offset = _bounds[pos] - _dim_bounds[pos];
+    }
+    else if(pos == "right"){
+      b = _bounds.bottomRight;
+      e = _bounds.topRight;
+      offset = _bounds[pos] - _dim_bounds[pos];
+    }
+
+    // если точки профиля еще не нарисованы - выходим
+    if(!b || !e){
+      this.visible = false;
+      return;
+    }
+
+    const tmp = new paper.Path({ insert: false, segments: [b, e] });
+
+    if(data.elm1 && pos){
+      b = tmp.getNearestPoint(data.elm1[data.p1]);
+      e = tmp.getNearestPoint(data.elm2[data.p2]);
+      if(tmp.getOffsetOf(b) > tmp.getOffsetOf(e)){
+        [b, e] = [e, b]
+      }
+      tmp.firstSegment.point = b;
+      tmp.lastSegment.point = e;
+    }
+
+    // прячем крошечные размеры
+    const length = tmp.length;
+    if(length < consts.sticking_l){
+      this.visible = false;
+      return;
+    }
+
+    this.visible = true;
+
+    const normal = tmp.getNormalAt(0).multiply(this.offset + offset);
+
+    bs = b.add(normal.multiply(0.8));
+    es = e.add(normal.multiply(0.8));
+
+    if(children.callout1.segments.length){
+      children.callout1.firstSegment.point = b;
+      children.callout1.lastSegment.point = b.add(normal);
+    }
+    else{
+      children.callout1.addSegments([b, b.add(normal)]);
+    }
+
+    if(children.callout2.segments.length){
+      children.callout2.firstSegment.point = e;
+      children.callout2.lastSegment.point = e.add(normal);
+    }
+    else{
+      children.callout2.addSegments([e, e.add(normal)]);
+    }
+
+    if(children.scale.segments.length){
+      children.scale.firstSegment.point = bs;
+      children.scale.lastSegment.point = es;
+    }
+    else{
+      children.scale.addSegments([bs, es]);
+    }
+
+    children.text.content = length.toFixed(0);
+    children.text.rotation = e.subtract(b).angle;
+    children.text.point = bs.add(es).divide(2);
+  }
+
+  // размер
+  get size() {
+    return parseFloat(this.children.text.content) || 0;
+  }
+  set size(v) {
+    this.children.text.content = parseFloat(v).round(1);
+  }
+
+  // угол к горизонту в направлении размера
+  get angle() {
+    return 0;
+  }
+  set angle(v) {
+
+  }
+
+  // расположение относительно контура $p.enm.pos
+  get pos() {
+    return this.data.pos || "";
+  }
+  set pos(v) {
+    this.data.pos = v;
+    this.redraw();
+  }
+
+  // отступ от внешней границы изделия
+  get offset() {
+    return this.data.offset || 90;
+  }
+  set offset(v) {
+    const offset = (parseInt(v) || 90).round(0);
+    if(this.data.offset != offset){
+      this.data.offset = offset;
+      this.project.register_change(true);
+    }
+  }
+
+  /**
+   * Удаляет элемент из контура и иерархии проекта
+   * Одновлеменно, удаляет строку из табчасти табчасти _Координаты_
+   * @method remove
+   */
+  remove() {
+    if(this._row){
+      this._row._owner.del(this._row);
+      this._row = null;
+      this.project.register_change();
+    }
+    super.remove();
+  }
 }
-DimensionLine._extend(paper.Group);
 
-DimensionLine.prototype.__define({
-
-	// виртуальные метаданные для автоформ
-	_metadata: {
-		get: function () {
-			return $p.dp.builder_text.metadata();
-		}
-	},
-
-	// виртуальный датаменеджер для автоформ
-	_manager: {
-		get: function () {
-			return $p.dp.builder_text;
-		}
-	},
-
-	_mouseenter: {
-		value: function (event) {
-			paper.canvas_cursor('cursor-arrow-ruler');
-		}
-	},
-
-	_mouseleave: {
-		value: function (event) {
-			//paper.canvas_cursor('cursor-arrow-white');
-		}
-	},
-
-	_click: {
-		value: function (event) {
-			event.stop();
-			this.wnd = new RulerWnd(null, this);
-			this.wnd.size = this.size;
-		}
-	},
-
-	_move_points: {
-		value: function (event, xy) {
-
-			var _bounds, delta, size;
-
-			// получаем дельту - на сколько смещать
-			if(this.data.elm1){
-
-				// в _bounds[event.name] надо поместить координату по x или у (в зависисмости от xy), которую будем двигать
-				_bounds = {};
-
-
-				if(this.pos == "top" || this.pos == "bottom"){
-
-					size = Math.abs(this.data.elm1[this.data.p1].x - this.data.elm2[this.data.p2].x);
-
-					if(event.name == "right"){
-						delta = new paper.Point(event.size - size, 0);
-						_bounds[event.name] = Math.max(this.data.elm1[this.data.p1].x, this.data.elm2[this.data.p2].x);
-
-					}else{
-						delta = new paper.Point(size - event.size, 0);
-						_bounds[event.name] = Math.min(this.data.elm1[this.data.p1].x, this.data.elm2[this.data.p2].x);
-					}
-
-
-				}else{
-
-					size = Math.abs(this.data.elm1[this.data.p1].y - this.data.elm2[this.data.p2].y);
-
-					if(event.name == "bottom"){
-						delta = new paper.Point(0, event.size - size);
-						_bounds[event.name] = Math.max(this.data.elm1[this.data.p1].y, this.data.elm2[this.data.p2].y);
-
-					}
-					else{
-						delta = new paper.Point(0, size - event.size);
-						_bounds[event.name] = Math.min(this.data.elm1[this.data.p1].y, this.data.elm2[this.data.p2].y);
-					}
-				}
-
-			}else {
-
-				_bounds = this.layer.bounds;
-
-				if(this.pos == "top" || this.pos == "bottom")
-					if(event.name == "right")
-						delta = new paper.Point(event.size - _bounds.width, 0);
-					else
-						delta = new paper.Point(_bounds.width - event.size, 0);
-				else{
-					if(event.name == "bottom")
-						delta = new paper.Point(0, event.size - _bounds.height);
-					else
-						delta = new paper.Point(0, _bounds.height - event.size);
-				}
-
-			}
-
-			if(delta.length){
-
-				paper.project.deselect_all_points();
-
-				paper.project.getItems({class: Profile}).forEach(function (p) {
-					if(Math.abs(p.b[xy] - _bounds[event.name]) < consts.sticking0 && Math.abs(p.e[xy] - _bounds[event.name]) < consts.sticking0){
-						p.generatrix.segments.forEach(function (segm) {
-							segm.selected = true;
-						})
-
-					}else if(Math.abs(p.b[xy] - _bounds[event.name]) < consts.sticking0){
-						p.generatrix.firstSegment.selected = true;
-
-					}else if(Math.abs(p.e[xy] - _bounds[event.name]) < consts.sticking0){
-						p.generatrix.lastSegment.selected = true;
-
-					}
-
-				});
-				this.project.move_points(delta);
-				setTimeout(function () {
-					this.deselect_all_points(true);
-					this.register_update();
-					//this.zoom_fit();
-				}.bind(this.project), 200);
-			}
-		}
-	},
-
-	_sizes_wnd: {
-		value: function (event) {
-      if(this.wnd && event.wnd == this.wnd.wnd){
-
-				switch(event.name) {
-					case 'close':
-						if(this.children.text){
-              this.children.text.selected = false;
-            }
-						this.wnd = null;
-						break;
-
-					case 'left':
-					case 'right':
-						if(this.pos == "top" || this.pos == "bottom"){
-              this._move_points(event, "x");
-            }
-						break;
-
-					case 'top':
-					case 'bottom':
-						if(this.pos == "left" || this.pos == "right"){
-              this._move_points(event, "y");
-            }
-						break;
-				}
-			}
-		}
-	},
-
-	redraw: {
-		value: function () {
-
-		  const {layer, project, data, pos} = this;
-      const _bounds = layer.bounds;
-      const _dim_bounds = layer instanceof DimensionLayer ? project.dimension_bounds : layer.dimension_bounds;
-			let offset = 0, b, e, tmp, normal, length, bs, es;
-
-			if(!pos){
-
-				b = typeof data.p1 == "number" ? data.elm1.corns(data.p1) : b = data.elm1[data.p1];
-        e = typeof data.p2 == "number" ? data.elm2.corns(data.p2) : e = data.elm2[data.p2];
-
-			}else if(pos == "top"){
-				b = _bounds.topLeft;
-				e = _bounds.topRight;
-				offset = _bounds[pos] - _dim_bounds[pos];
-
-			}else if(pos == "left"){
-				b = _bounds.bottomLeft;
-				e = _bounds.topLeft;
-				offset = _bounds[pos] - _dim_bounds[pos];
-
-			}else if(pos == "bottom"){
-				b = _bounds.bottomLeft;
-				e = _bounds.bottomRight;
-				offset = _bounds[pos] - _dim_bounds[pos];
-
-			}else if(pos == "right"){
-				b = _bounds.bottomRight;
-				e = _bounds.topRight;
-				offset = _bounds[pos] - _dim_bounds[pos];
-			}
-
-			// если точки профиля еще не нарисованы - выходим
-			if(!b || !e){
-				this.visible = false;
-				return;
-			}
-
-			tmp = new paper.Path({ insert: false, segments: [b, e] });
-
-			if(data.elm1 && pos){
-
-				b = tmp.getNearestPoint(data.elm1[data.p1]);
-				e = tmp.getNearestPoint(data.elm2[data.p2]);
-				if(tmp.getOffsetOf(b) > tmp.getOffsetOf(e)){
-					normal = e;
-					e = b;
-					b = normal;
-				}
-				tmp.firstSegment.point = b;
-				tmp.lastSegment.point = e;
-
-			};
-
-			// прячем крошечные размеры
-			length = tmp.length;
-			if(length < consts.sticking_l){
-				this.visible = false;
-				return;
-			}
-
-			this.visible = true;
-
-			normal = tmp.getNormalAt(0).multiply(this.offset + offset);
-
-			bs = b.add(normal.multiply(0.8));
-			es = e.add(normal.multiply(0.8));
-
-			if(this.children.callout1.segments.length){
-				this.children.callout1.firstSegment.point = b;
-				this.children.callout1.lastSegment.point = b.add(normal);
-			}else
-				this.children.callout1.addSegments([b, b.add(normal)]);
-
-			if(this.children.callout2.segments.length){
-				this.children.callout2.firstSegment.point = e;
-				this.children.callout2.lastSegment.point = e.add(normal);
-			}else
-				this.children.callout2.addSegments([e, e.add(normal)]);
-
-			if(this.children.scale.segments.length){
-				this.children.scale.firstSegment.point = bs;
-				this.children.scale.lastSegment.point = es;
-			}else
-				this.children.scale.addSegments([bs, es]);
-
-			this.children.text.content = length.toFixed(0);
-			this.children.text.rotation = e.subtract(b).angle;
-			this.children.text.point = bs.add(es).divide(2);
-
-		},
-		enumerable : false
-	},
-
-	// размер
-	size: {
-		get: function () {
-			return parseFloat(this.children.text.content);
-		},
-		set: function (v) {
-			this.children.text.content = parseFloat(v).round(1);
-		}
-	},
-
-	// угол к горизонту в направлении размера
-	angle: {
-		get: function () {
-			return 0;
-		},
-		set: function (v) {
-
-		}
-	},
-
-	// расположение относительно контура $p.enm.pos
-	pos: {
-		get: function () {
-			return this.data.pos || "";
-		},
-		set: function (v) {
-			this.data.pos = v;
-			this.redraw();
-		}
-	},
-
-	// отступ от внешней границы изделия
-	offset: {
-		get: function () {
-			return this.data.offset || 90;
-		},
-		set: function (v) {
-			var offset = (parseInt(v) || 90).round(0);
-			if(this.data.offset != offset){
-				this.data.offset = offset;
-				this.project.register_change(true);
-			}
-		}
-	}
-
-});
 
 /**
  * ### Служебный слой размерных линий
@@ -464,7 +435,7 @@ class DimensionLineCustom extends DimensionLine {
    * @method save_coordinates
    */
   save_coordinates() {
-    var _row = this._row;
+    const _row = this._row;
 
     // сохраняем размер
     _row.len = this.size;
