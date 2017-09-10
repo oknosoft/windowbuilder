43c7989eb580982a830f3d60cfb0dc5535a6b417
unpete <info@oknosoft.ru>
2017-09-11 02:57:42 +0500

    экспорт табчасти в xls и буфер обмена
    https://github.com/oknosoft/metadata.js/commit/6e28ccb6ae786e523c02bb68a03ae8210636b60d

--- src/modifiers/catalogs/cat_characteristics_form_obj.js
+++ src/modifiers/catalogs/cat_characteristics_form_obj.js
@@ -11,16 +11,18 @@ $p.cat.characteristics.form_obj = function (pwnd, attr) {
         tabular_init('specification', $p.injected_data['toolbar_characteristics_specification.xml']);
         wnd.elmnts.tabs.tab_specification.getAttachedToolbar().attachEvent('onclick', (btn_id) => {
 
-          const selId = wnd.elmnts.grids.specification.getSelectedRowId();
-          if(selId && !isNaN(Number(selId))) {
-            return o.open_origin(Number(selId) - 1);
-          }
+          if(btn_id == 'btn_origin') {
+            const selId = wnd.elmnts.grids.specification.getSelectedRowId();
+            if(selId && !isNaN(Number(selId))) {
+              return o.open_origin(Number(selId) - 1);
+            }
 
-          $p.msg.show_msg({
-            type: 'alert-warning',
-            text: $p.msg.no_selected_row.replace('%1', 'Спецификация'),
-            title: o.presentation
-          });
+            $p.msg.show_msg({
+              type: 'alert-warning',
+              text: $p.msg.no_selected_row.replace('%1', 'Спецификация'),
+              title: o.presentation
+            });
+          }
 
         });
       }
