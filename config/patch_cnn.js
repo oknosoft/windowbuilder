71b38807ebc6bb861d21e5243c2abf484bb20fa6
unpete <info@oknosoft.ru>
2017-11-02 14:28:10 +0500

    ошибка в reset_replace(prm)

--- config/patch_cnn.js
+++ config/patch_cnn.js
@@ -3,21 +3,22 @@ function reset_replace(prm) {
 
   const {pouch} = $p.wsql;
   const {local} = pouch;
-  const destroy_ram = local.ram.destroy.bind(local.ram);
-  const destroy_doc = local.doc.destroy.bind(local.doc);
+  const destroy_ram = local.ram && local.ram.destroy.bind(local.ram);
+  const destroy_doc = local.doc && local.doc.destroy.bind(local.doc);
   const do_reload = () => {
     setTimeout(() => {
       $p.eve.redirect = true;
       location.replace(prm.host);
     }, 1000);
   };
-  const do_replace = () => {
-    destroy_ram()
+  const do_replace = destroy_ram && destroy_ram ?
+    () => destroy_ram()
       .then(destroy_doc)
       .catch(destroy_doc)
       .then(do_reload)
-      .catch(do_reload);
-  }
+      .catch(do_reload)
+    :
+    do_reload;
 
   setTimeout(do_replace, 10000);
 
