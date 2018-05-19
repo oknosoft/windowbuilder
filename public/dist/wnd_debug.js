b738436ccbc06678d24998ceb65202d41bf1d895
unpete <info@oknosoft.ru>
2018-05-19 15:10:57 +0500

    оптимизация поиска по строке

--- public/dist/wnd_debug.js
+++ public/dist/wnd_debug.js
@@ -2900,7 +2900,7 @@ class Pricing {
               since: 'now',
               live: true,
               include_docs: true,
-              selector: {class_name: {$in: ['doc.nom_prices_setup', 'doc.calc_order', 'cat.formulas']}}
+              selector: {class_name: {$in: ['doc.nom_prices_setup', 'doc.calc_order']}}
             }).on('change', (change) => {
               if(change.doc.class_name == 'doc.nom_prices_setup'){
                 setTimeout(() => {
@@ -4379,228 +4379,6 @@ $p.spec_building = new SpecBuilding($p);
 })($p.classes.DataManager);
 
 
-(function(_mgr){
-
-	const acn = {
-    ii: [_mgr.Наложение],
-    i: [_mgr.НезамкнутыйКонтур],
-    a: [
-      _mgr.УгловоеДиагональное,
-      _mgr.УгловоеКВертикальной,
-      _mgr.УгловоеКГоризонтальной,
-      _mgr.КрестВСтык],
-    t: [_mgr.ТОбразное, _mgr.КрестВСтык],
-	};
-
-
-	Object.defineProperties(_mgr, {
-	  ad: {
-	    get() {
-        return this.УгловоеДиагональное;
-      }
-    },
-    av: {
-      get() {
-        return this.УгловоеКВертикальной;
-      }
-    },
-    ah: {
-      get() {
-        return this.УгловоеКГоризонтальной;
-      }
-    },
-    t: {
-      get() {
-        return this.ТОбразное;
-      }
-    },
-    ii: {
-      get() {
-        return this.Наложение;
-      }
-    },
-    i: {
-      get() {
-        return this.НезамкнутыйКонтур;
-      }
-    },
-    xt: {
-      get() {
-        return this.КрестПересечение;
-      }
-    },
-    xx: {
-      get() {
-        return this.КрестВСтык;
-      }
-    },
-
-    acn: {
-      value: acn
-    },
-
-  });
-
-})($p.enm.cnn_types);
-
-
-(function(_mgr){
-
-	const cache = {};
-
-	_mgr.__define({
-
-		profiles: {
-			get(){
-				return cache.profiles
-					|| ( cache.profiles = [
-						_mgr.Рама,
-						_mgr.Створка,
-						_mgr.Импост,
-						_mgr.Штульп] );
-			}
-		},
-
-		profile_items: {
-			get(){
-				return cache.profile_items
-					|| ( cache.profile_items = [
-						_mgr.Рама,
-						_mgr.Створка,
-						_mgr.Импост,
-						_mgr.Штульп,
-						_mgr.Добор,
-						_mgr.Соединитель,
-						_mgr.Раскладка
-					] );
-			}
-		},
-
-		rama_impost: {
-			get(){
-				return cache.rama_impost
-					|| ( cache.rama_impost = [ _mgr.Рама, _mgr.Импост] );
-			}
-		},
-
-		impost_lay: {
-			get(){
-				return cache.impost_lay
-					|| ( cache.impost_lay = [ _mgr.Импост, _mgr.Раскладка] );
-			}
-		},
-
-		stvs: {
-			get(){
-				return cache.stvs || ( cache.stvs = [_mgr.Створка] );
-			}
-		},
-
-		glasses: {
-			get(){
-				return cache.glasses
-					|| ( cache.glasses = [ _mgr.Стекло, _mgr.Заполнение] );
-			}
-		}
-
-	});
-
-
-})($p.enm.elm_types);
-
-
-
-(function(_mgr){
-
-  _mgr.additions_groups = [_mgr.Подоконник, _mgr.Водоотлив, _mgr.МоскитнаяСетка, _mgr.Откос, _mgr.Профиль, _mgr.Монтаж, _mgr.Доставка, _mgr.Набор];
-
-
-})($p.enm.inserts_types);
-
-
-
-(function($p){
-
-	$p.enm.open_types.__define({
-
-		is_opening: {
-			value(v) {
-
-				if(!v || v.empty() || v == this.Глухое || v == this.Неподвижное)
-					return false;
-
-				return true;
-
-			}
-		}
-
-
-	});
-
-	$p.enm.orientations.__define({
-
-		hor: {
-			get() {
-				return this.Горизонтальная;
-			}
-		},
-
-		vert: {
-			get() {
-				return this.Вертикальная;
-			}
-		},
-
-		incline: {
-			get() {
-				return this.Наклонная;
-			}
-		}
-	});
-
-	$p.enm.positions.__define({
-
-		left: {
-			get() {
-				return this.Лев;
-			}
-		},
-
-		right: {
-			get() {
-				return this.Прав;
-			}
-		},
-
-		top: {
-			get() {
-				return this.Верх;
-			}
-		},
-
-		bottom: {
-			get() {
-				return this.Низ;
-			}
-		},
-
-		hor: {
-			get() {
-				return this.ЦентрГоризонталь;
-			}
-		},
-
-		vert: {
-			get() {
-				return this.ЦентрВертикаль;
-			}
-		}
-	});
-
-
-})($p);
-
-
 $p.DocCalc_order = class DocCalc_order extends $p.DocCalc_order {
 
 
@@ -5707,202 +5485,193 @@ $p.doc.calc_order.form_list = function(pwnd, attr, handlers){
 		};
 	}
 
-  return this.pouch_db.getIndexes()
-    .then(({indexes}) => {
-      attr._index = {
-        ddoc: 'mango_calc_order',
-        fields: ['department', 'state', 'date', 'search'],
-        name: 'list',
-        type: 'json',
-      };
-      if(!indexes.some(({ddoc}) => ddoc && ddoc.indexOf(attr._index.ddoc) != -1)){
-        return this.pouch_db.createIndex(attr._index);
+  return new Promise((resolve, reject) => {
+
+    attr._index = {
+      ddoc: 'mango_calc_order/list',
+      fields: ['department', 'state', 'date', 'search']
+    };
+
+    attr.on_create = (wnd) => {
+
+      const {elmnts} = wnd;
+
+      wnd.dep_listener = (obj, fields) => {
+        if(obj == dp && fields.department){
+          elmnts.filter.call_event();
+          $p.wsql.set_user_param('current_department', dp.department.ref);
+        }
       }
-    })
-    .then(() => {
-      return new Promise((resolve, reject) => {
 
-        attr.on_create = (wnd) => {
-
-          const {elmnts} = wnd;
-
-          wnd.dep_listener = (obj, fields) => {
-            if(obj == dp && fields.department){
-              elmnts.filter.call_event();
-              $p.wsql.set_user_param('current_department', dp.department.ref);
-            }
-          }
-
-          if(handlers){
-            const {custom_selection} = elmnts.filter;
-            custom_selection._state = handlers.props.state_filter;
-            custom_selection.class_name = 'doc.calc_order';
-            handlers.onProps = (props) => {
-              if(custom_selection._state != props.state_filter){
-                custom_selection._state = props.state_filter;
-                elmnts.filter.call_event();
-              }
-              if(elmnts.toolbar) {
-                if(custom_selection._state === 'draft') {
-                  elmnts.toolbar.enableItem('btn_delete');
-                }
-                else {
-                  elmnts.toolbar.disableItem('btn_delete');
-                }
-              }
-            }
-
-            wnd.handleNavigate = handlers.handleNavigate;
-            wnd.handleIfaceState = handlers.handleIfaceState;
-          }
-
-          const dp = $p.dp.builder_price.create();
-          const pos = elmnts.toolbar.getPosition('input_filter');
-
-          elmnts.toolbar.addButtonTwoState('by_number', pos, '<i class="fa fa-key fa-fw"></i>');
-          if($p.wsql.get_user_param('calc_order_by_number', 'boolean')) {
-            elmnts.toolbar.setItemState('by_number', true);
-          }
-          elmnts.toolbar.setItemToolTip('by_number', 'Режим поиска с учетом либо без учета статуса и подразделения');
-          elmnts.toolbar.attachEvent('onStateChange', (id, state) => {
-            $p.wsql.set_user_param('calc_order_by_number', state);
+      if(handlers){
+        const {custom_selection} = elmnts.filter;
+        custom_selection._state = handlers.props.state_filter;
+        custom_selection.class_name = 'doc.calc_order';
+        handlers.onProps = (props) => {
+          if(custom_selection._state != props.state_filter){
+            custom_selection._state = props.state_filter;
             elmnts.filter.call_event();
-          });
-
-          const txt_id = `txt_${dhx4.newId()}`;
-          elmnts.toolbar.addText(txt_id, pos, '');
-          const txt_div = elmnts.toolbar.objPull[elmnts.toolbar.idPrefix + txt_id].obj;
-          const dep = new $p.iface.OCombo({
-            parent: txt_div,
-            obj: dp,
-            field: 'department',
-            width: 180,
-            hide_frm: true,
-          });
-          txt_div.style.border = '1px solid #ccc';
-          txt_div.style.borderRadius = '3px';
-          txt_div.style.padding = '3px 2px 1px 2px';
-          txt_div.style.margin = '1px 5px 1px 1px';
-          dep.DOMelem_input.placeholder = 'Подразделение';
-
-          dp._manager.on('update', wnd.dep_listener);
-
-          const set_department = $p.DocCalc_order.set_department.bind(dp);
-          set_department();
-          if(!$p.wsql.get_user_param('couch_direct')){
-            $p.md.once('user_log_in', set_department);
           }
-
-          elmnts.filter.custom_selection.__define({
-            department: {
-              get() {
-                const {department} = dp;
-                return this._state == 'template' ? {$eq: $p.utils.blank.guid} : {$eq: department.ref};
-              },
-              enumerable: true
-            },
-            state: {
-              get(){
-                return this._state == 'all' ? {$in: 'draft,sent,confirmed,declined,service,complaints,template,zarchive'.split(',')} : {$eq: this._state};
-              },
-              enumerable: true
-            },
-
-            _sort: {
-              get() {
-                if($p.wsql.get_user_param('calc_order_by_number', 'boolean')) {
-                  const flt = elmnts.filter.get_filter();
-                  if(flt.filter.length > 5) {
-                    return [{class_name: 'desc'}, {date: 'desc'}, {search: 'desc'}];
-                  }
-                }
-                return [{department: 'desc'}, {state: 'desc'}, {date: 'desc'}];
-              }
-            },
-
-            _index: {
-              get() {
-                if($p.wsql.get_user_param('calc_order_by_number', 'boolean')) {
-                  const flt = elmnts.filter.get_filter();
-                  if(flt.filter.length > 5) {
-                    return {
-                      ddoc: 'mango',
-                      fields: ['class_name', 'date', 'search']
-                    };
-                  }
-                }
-                return attr._index;
-              }
-            },
-
-          });
-
-          elmnts.status_bar = wnd.attachStatusBar();
-          elmnts.svgs = new $p.iface.OSvgs(wnd, elmnts.status_bar,
-            (ref, dbl) => {
-              dbl && handlers.handleNavigate(`/builder/${ref}`);
-            });
-          elmnts.grid.attachEvent('onRowSelect', (rid) => elmnts.svgs.reload(rid));
-
-          wnd.attachEvent('onClose', (win) => {
-            dep && dep.unload();
-            return true;
-          });
-
-          attr.on_close = () => {
-            elmnts.svgs && elmnts.svgs.unload();
-            dep && dep.unload();
-          }
-
-
-
-          attr.toolbar_click = function toolbar_click(btn_id) {
-            switch (btn_id) {
-
-            case 'calc_order':
-              const ref = wnd.elmnts.grid.getSelectedRowId();
-              if(ref) {
-                const {calc_order} = $p.doc;
-                calc_order.clone(ref)
-                  .then((doc) => {
-                    handlers.handleNavigate(`/${calc_order.class_name}/${doc.ref}`);
-                  })
-                  .catch($p.record_log);
-                ;
-              }
-              else {
-                $p.msg.show_msg({
-                  type: 'alert-warning',
-                  text: $p.msg.no_selected_row.replace('%1', ''),
-                  title: $p.msg.main_title
-                });
-              }
-              break;
-
-            case 'btn_templates':
-            case 'btn_download':
-            case 'btn_share':
-            case 'btn_inbox':
-              $p.dp.buyers_order.open_component(wnd, {
-                ref: wnd.elmnts.grid.getSelectedRowId(),
-                cmd: btn_id
-              }, handlers, 'PushUtils', 'CalcOrderList');
-              break;
-
+          if(elmnts.toolbar) {
+            if(custom_selection._state === 'draft') {
+              elmnts.toolbar.enableItem('btn_delete');
+            }
+            else {
+              elmnts.toolbar.disableItem('btn_delete');
             }
           }
-
-          resolve(wnd);
         }
 
-        attr.toolbar_struct = $p.injected_data['toolbar_calc_order_selection.xml'];
+        wnd.handleNavigate = handlers.handleNavigate;
+        wnd.handleIfaceState = handlers.handleIfaceState;
+      }
 
+      const dp = $p.dp.builder_price.create();
+      const pos = elmnts.toolbar.getPosition('input_filter');
 
+      elmnts.toolbar.addButtonTwoState('by_number', pos, '<i class="fa fa-key fa-fw"></i>');
+      if($p.wsql.get_user_param('calc_order_by_number', 'boolean')) {
+        elmnts.toolbar.setItemState('by_number', true);
+      }
+      elmnts.toolbar.setItemToolTip('by_number', 'Режим поиска с учетом либо без учета статуса и подразделения');
+      elmnts.toolbar.attachEvent('onStateChange', (id, state) => {
+        $p.wsql.set_user_param('calc_order_by_number', state);
+        elmnts.filter.call_event();
+      });
 
-        return this.mango_selection(pwnd, attr);
+      const txt_id = `txt_${dhx4.newId()}`;
+      elmnts.toolbar.addText(txt_id, pos, '');
+      const txt_div = elmnts.toolbar.objPull[elmnts.toolbar.idPrefix + txt_id].obj;
+      const dep = new $p.iface.OCombo({
+        parent: txt_div,
+        obj: dp,
+        field: 'department',
+        width: 180,
+        hide_frm: true,
+      });
+      txt_div.style.border = '1px solid #ccc';
+      txt_div.style.borderRadius = '3px';
+      txt_div.style.padding = '3px 2px 1px 2px';
+      txt_div.style.margin = '1px 5px 1px 1px';
+      dep.DOMelem_input.placeholder = 'Подразделение';
+
+      dp._manager.on('update', wnd.dep_listener);
+
+      const set_department = $p.DocCalc_order.set_department.bind(dp);
+      set_department();
+      if(!$p.wsql.get_user_param('couch_direct')){
+        $p.md.once('user_log_in', set_department);
+      }
+
+      elmnts.filter.custom_selection.__define({
+        department: {
+          get() {
+            const {department} = dp;
+            return this._state == 'template' ? {$eq: $p.utils.blank.guid} : {$eq: department.ref};
+          },
+          enumerable: true
+        },
+        state: {
+          get(){
+            return this._state == 'all' ? {$in: 'draft,sent,confirmed,declined,service,complaints,template,zarchive'.split(',')} : {$eq: this._state};
+          },
+          enumerable: true
+        },
+
+        _sort: {
+          get() {
+            if($p.wsql.get_user_param('calc_order_by_number', 'boolean')) {
+              const flt = elmnts.filter.get_filter();
+              if(flt.filter.length > 5) {
+                return [{class_name: 'desc'}, {date: 'desc'}, {search: 'desc'}];
+              }
+            }
+            return [{department: 'desc'}, {state: 'desc'}, {date: 'desc'}];
+          }
+        },
+
+        _index: {
+          get() {
+            if($p.wsql.get_user_param('calc_order_by_number', 'boolean')) {
+              const flt = elmnts.filter.get_filter();
+              if(flt.filter.length > 5) {
+                return {
+                  ddoc: 'mango/search',
+                  fields: ['class_name', 'date', 'search']
+                };
+              }
+            }
+            return attr._index;
+          }
+        },
 
       });
-    });
+
+      elmnts.status_bar = wnd.attachStatusBar();
+      elmnts.svgs = new $p.iface.OSvgs(wnd, elmnts.status_bar,
+        (ref, dbl) => {
+          dbl && handlers.handleNavigate(`/builder/${ref}`);
+        });
+      elmnts.grid.attachEvent('onRowSelect', (rid) => elmnts.svgs.reload(rid));
+
+      wnd.attachEvent('onClose', (win) => {
+        dep && dep.unload();
+        return true;
+      });
+
+      attr.on_close = () => {
+        elmnts.svgs && elmnts.svgs.unload();
+        dep && dep.unload();
+      }
+
+
+
+      attr.toolbar_click = function toolbar_click(btn_id) {
+        switch (btn_id) {
+
+        case 'calc_order':
+          const ref = wnd.elmnts.grid.getSelectedRowId();
+          if(ref) {
+            const {calc_order} = $p.doc;
+            calc_order.clone(ref)
+              .then((doc) => {
+                handlers.handleNavigate(`/${calc_order.class_name}/${doc.ref}`);
+              })
+              .catch($p.record_log);
+            ;
+          }
+          else {
+            $p.msg.show_msg({
+              type: 'alert-warning',
+              text: $p.msg.no_selected_row.replace('%1', ''),
+              title: $p.msg.main_title
+            });
+          }
+          break;
+
+        case 'btn_templates':
+        case 'btn_download':
+        case 'btn_share':
+        case 'btn_inbox':
+          $p.dp.buyers_order.open_component(wnd, {
+            ref: wnd.elmnts.grid.getSelectedRowId(),
+            cmd: btn_id
+          }, handlers, 'PushUtils', 'CalcOrderList');
+          break;
+
+        }
+      }
+
+      resolve(wnd);
+    }
+
+    attr.toolbar_struct = $p.injected_data['toolbar_calc_order_selection.xml'];
+
+
+
+    return this.mango_selection(pwnd, attr);
+
+  });
 
 };
 
@@ -7159,6 +6928,228 @@ $p.DocSelling.prototype.before_save = function () {
 };
 
 
+
+
+(function(_mgr){
+
+	const acn = {
+    ii: [_mgr.Наложение],
+    i: [_mgr.НезамкнутыйКонтур],
+    a: [
+      _mgr.УгловоеДиагональное,
+      _mgr.УгловоеКВертикальной,
+      _mgr.УгловоеКГоризонтальной,
+      _mgr.КрестВСтык],
+    t: [_mgr.ТОбразное, _mgr.КрестВСтык],
+	};
+
+
+	Object.defineProperties(_mgr, {
+	  ad: {
+	    get() {
+        return this.УгловоеДиагональное;
+      }
+    },
+    av: {
+      get() {
+        return this.УгловоеКВертикальной;
+      }
+    },
+    ah: {
+      get() {
+        return this.УгловоеКГоризонтальной;
+      }
+    },
+    t: {
+      get() {
+        return this.ТОбразное;
+      }
+    },
+    ii: {
+      get() {
+        return this.Наложение;
+      }
+    },
+    i: {
+      get() {
+        return this.НезамкнутыйКонтур;
+      }
+    },
+    xt: {
+      get() {
+        return this.КрестПересечение;
+      }
+    },
+    xx: {
+      get() {
+        return this.КрестВСтык;
+      }
+    },
+
+    acn: {
+      value: acn
+    },
+
+  });
+
+})($p.enm.cnn_types);
+
+
+(function(_mgr){
+
+	const cache = {};
+
+	_mgr.__define({
+
+		profiles: {
+			get(){
+				return cache.profiles
+					|| ( cache.profiles = [
+						_mgr.Рама,
+						_mgr.Створка,
+						_mgr.Импост,
+						_mgr.Штульп] );
+			}
+		},
+
+		profile_items: {
+			get(){
+				return cache.profile_items
+					|| ( cache.profile_items = [
+						_mgr.Рама,
+						_mgr.Створка,
+						_mgr.Импост,
+						_mgr.Штульп,
+						_mgr.Добор,
+						_mgr.Соединитель,
+						_mgr.Раскладка
+					] );
+			}
+		},
+
+		rama_impost: {
+			get(){
+				return cache.rama_impost
+					|| ( cache.rama_impost = [ _mgr.Рама, _mgr.Импост] );
+			}
+		},
+
+		impost_lay: {
+			get(){
+				return cache.impost_lay
+					|| ( cache.impost_lay = [ _mgr.Импост, _mgr.Раскладка] );
+			}
+		},
+
+		stvs: {
+			get(){
+				return cache.stvs || ( cache.stvs = [_mgr.Створка] );
+			}
+		},
+
+		glasses: {
+			get(){
+				return cache.glasses
+					|| ( cache.glasses = [ _mgr.Стекло, _mgr.Заполнение] );
+			}
+		}
+
+	});
+
+
+})($p.enm.elm_types);
+
+
+
+(function(_mgr){
+
+  _mgr.additions_groups = [_mgr.Подоконник, _mgr.Водоотлив, _mgr.МоскитнаяСетка, _mgr.Откос, _mgr.Профиль, _mgr.Монтаж, _mgr.Доставка, _mgr.Набор];
+
+
+})($p.enm.inserts_types);
+
+
+
+(function($p){
+
+	$p.enm.open_types.__define({
+
+		is_opening: {
+			value(v) {
+
+				if(!v || v.empty() || v == this.Глухое || v == this.Неподвижное)
+					return false;
+
+				return true;
+
+			}
+		}
+
+
+	});
+
+	$p.enm.orientations.__define({
+
+		hor: {
+			get() {
+				return this.Горизонтальная;
+			}
+		},
+
+		vert: {
+			get() {
+				return this.Вертикальная;
+			}
+		},
+
+		incline: {
+			get() {
+				return this.Наклонная;
+			}
+		}
+	});
+
+	$p.enm.positions.__define({
+
+		left: {
+			get() {
+				return this.Лев;
+			}
+		},
+
+		right: {
+			get() {
+				return this.Прав;
+			}
+		},
+
+		top: {
+			get() {
+				return this.Верх;
+			}
+		},
+
+		bottom: {
+			get() {
+				return this.Низ;
+			}
+		},
+
+		hor: {
+			get() {
+				return this.ЦентрГоризонталь;
+			}
+		},
+
+		vert: {
+			get() {
+				return this.ЦентрВертикаль;
+			}
+		}
+	});
+
+
+})($p);
 
 
 (($p) => {
