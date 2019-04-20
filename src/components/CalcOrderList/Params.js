bb8846a970c3f85635f6701dab7925243cd94ab6
unpete <info@oknosoft.ru>
2019-04-20 16:51:38 +0500

    Фильтр по статусу, подразделению и менеджеру

--- src/components/CalcOrderList/Params.js
+++ src/components/CalcOrderList/Params.js
@@ -11,14 +11,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import FormGroup from '@material-ui/core/FormGroup';
 import DataField from 'metadata-react/DataField';
-import SelectTags from 'metadata-react/DataField/SelectTags';
+import ChipList from 'metadata-react/DataField/ChipList';
 import withStyles from '@material-ui/core/styles/withStyles';
+
 
 const styles = theme => ({
   group: {
     marginLeft: theme.spacing.unit,
   },
 });
+
+const sort = (a, b) => {
+  if (a.name < b.name){
+    return -1;
+  }
+  else if (a.name > b.name){
+    return 1;
+  }
+  return 0;
+};
 
 class Params extends React.Component {
 
@@ -47,36 +58,44 @@ class Params extends React.Component {
 
     const {current_user, wsql, cat} = $p;
     const current_department = wsql.get_user_param('current_department');
-    const department = this.department = [
 
-    ];
+    const department = this.department = [];
     department._mgr = cat.divisions;
+
+    const manager = this.manager = [];
+    manager._mgr = cat.users;
+
     if(current_user && !current_user.branch.empty()) {
       current_user.branch.divisions.forEach((v) => {
         department.push(v.acl_obj);
       });
-    }
-    else {
-      cat.divisions.forEach((v) => {
-        department.push(v);
+      cat.users.find_rows({branch: current_user.branch}, (v) => {
+        manager.push(v);
       });
     }
+    else {
+      cat.divisions.forEach(department.push.bind(department));
+      cat.users.forEach(manager.push.bind(manager));
+    }
+
 
     this.state = {
       obj_delivery_state: [states[0].ref],
       department: [],
+      manager: [],
     }
+
     if(department.length){
-      department.sort((a, b) => {
-        if (a.name < b.name){
-          return -1;
-        }
-        else if (a.name > b.name){
-          return 1;
-        }
-        return 0;
-      });
+      department.sort(sort);
       this.state.department.push(current_department && department.some((v) => v == current_department) ? current_department : department[0].ref);
+    }
+
+    if(manager.length){
+      manager.sort(sort);
+      if(current_user && !current_user.branch.empty()) {
+        this.state.manager.push(current_user.ref);
+        this.apply_ref_filter('manager', this.state.manager);
+      }
     }
   }
 
@@ -136,27 +155,32 @@ class Params extends React.Component {
 
   render() {
     const {scheme, handleFilterChange, classes} = this.props;
-    const {obj_delivery_state, department} = this.state;
+    const {obj_delivery_state, department, manager} = this.state;
     return <div className={classes.group}>
       <FormGroup key="dates" row>
         <DataField _obj={scheme} _fld="date_from"/>
         <DataField _obj={scheme} _fld="date_till"/>
       </FormGroup>
-      <SelectTags
+      <ChipList
         title="Статусы"
-        tags={obj_delivery_state}
-        fullWidth
+        items={this.obj_delivery_state}
+        selectedItems={obj_delivery_state}
         handleChange={this.handleChange('obj_delivery_state')}
-        tagList={this.obj_delivery_state}
-        _mgr={this.obj_delivery_state._mgr}
-      />
-      <SelectTags
-        title="Подразделения"
-        tags={department}
         fullWidth
+      />
+      <ChipList
+        title="Подразделения"
+        items={this.department}
+        selectedItems={department}
         handleChange={this.handleChange('department')}
-        tagList={this.department}
-        _mgr={this.department._mgr}
+        fullWidth
+      />
+      <ChipList
+        title="Менеджер"
+        items={this.manager}
+        selectedItems={manager}
+        handleChange={this.handleChange('manager')}
+        fullWidth
       />
     </div>;
   }
