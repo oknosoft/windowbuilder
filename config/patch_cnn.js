f1eed1c33149e1ecf3529870db610994f1128fa5
unpete <info@oknosoft.ru>
2019-04-23 20:04:12 +0500

    индивидуальный google_maps_api_key

--- config/patch_cnn.js
+++ config/patch_cnn.js
@@ -36,10 +36,25 @@ function reset_replace(prm) {
 export const predefined = {
   'aribaz.': {zone: 2, host: 'https://aribaz.oknosoft.ru/'},
   'eco.': {zone: 21, host: 'https://eco.oknosoft.ru/'},
-  'ecookna.': {zone: 21, host: 'https://zakaz.ecookna.ru/', splash: {css: 'splash21', title: false}, log_level: 'warn', templates: true},
+  'ecookna.': {
+    zone: 21,
+    host: 'https://zakaz.ecookna.ru/',
+    splash: {css: 'splash21', title: false},
+    log_level: 'warn',
+    templates: true,
+    use_google_geo: 'AIzaSyCpDPeBnIbhNJuCiq_WG58lVUHF_5nDKSQ',
+  },
   //'localhost': {zone: 21, splash: {img: '', text: ''}, splash: {css: 'splash21', title: false}, log_level: 'warn', templates: true}, //
-  'rusokon.': {zone: 19, host: 'https://rusokon.oknosoft.ru/'},
-  'kaleva.': {zone: 8, host: 'https://zakaz.kaleva.ru/'},
+  'rusokon.': {
+    zone: 19,
+    host: 'https://rusokon.oknosoft.ru/',
+    use_google_geo: 'AIzaSyCpDPeBnIbhNJuCiq_WG58lVUHF_5nDKSQ',
+  },
+  'kaleva.': {
+    zone: 8,
+    host: 'https://zakaz.kaleva.ru/',
+    use_google_geo: 'AIzaSyCpDPeBnIbhNJuCiq_WG58lVUHF_5nDKSQ',
+  },
   'tmk.': {zone: 23, host: 'https://tmk-online.ru/'},
   'crystallit.': {zone: 25, host: 'https://crystallit.oknosoft.ru/'},
   'okna-stolicy.': {zone: 22, host: 'https://okna-stolicy.oknosoft.ru/'},
@@ -72,15 +87,11 @@ export function patch_cnn() {
     const prm = predefined[elm];
     if(location.host.match(elm)) {
       wsql.get_user_param('zone') != prm.zone && wsql.set_user_param('zone', prm.zone);
-      if(prm.log_level) {
-        job_prm.job_prm = prm.log_level;
-      }
-      if(prm.splash) {
-        job_prm.splash = prm.splash;
-      }
-      if(prm.templates) {
-        job_prm.templates = prm.templates;
-      }
+      'log_level,splash,templates,use_google_geo'.split(',').forEach((name) => {
+        if(prm.hasOwnProperty(name)) {
+          job_prm[name] = prm[name];
+        }
+      });
     }
   }
   if(!location.host.match('localhost')) {
