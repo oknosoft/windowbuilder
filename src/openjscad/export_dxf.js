684085c37beefee74b585d2fef6182c22a8dd876
unpete <info@oknosoft.ru>
2019-05-07 09:39:52 +0500

    dxf (jscad.default || jscad)

--- src/openjscad/export_dxf.js
+++ src/openjscad/export_dxf.js
@@ -8,6 +8,6 @@ export default function ($p) {
     //import('@jscad/openjscad')
     import('dxf-writer/src/Drawing')
       .then((jscad) => import('./exec_js_dxf')
-        .then(({exec_dxf}) => exec_dxf(scheme, jscad)));
+        .then(({exec_dxf}) => exec_dxf(scheme, jscad.default || jscad)));
   });
 }
