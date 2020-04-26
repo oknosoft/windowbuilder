266f6f8ee904a7cfd55140ae093dda7ae1b5c4e2
unpete <info@oknosoft.ru>
2020-04-26 19:51:43 +0500

    методы attache_wnd detache_wnd переехали из ядра в клиента

--- public/dist/windowbuilder.js
+++ public/dist/windowbuilder.js
@@ -2244,6 +2244,39 @@ class Editor extends $p.EditorInvisible {
 $p.Editor = Editor;
 
 
+Editor.BuilderElement.prototype.attache_wnd = function attache_wnd(cell) {
+  if(!this._attr._grid || !this._attr._grid.cell){
+
+    this._attr._grid = cell.attachHeadFields({
+      obj: this,
+      oxml: this.oxml
+    });
+    this._attr._grid.attachEvent('onRowSelect', function (id) {
+      if(['x1', 'y1', 'a1', 'cnn1'].indexOf(id) != -1) {
+        this._obj.select_node('b');
+      }
+      else if(['x2', 'y2', 'a2', 'cnn2'].indexOf(id) != -1) {
+        this._obj.select_node('e');
+      }
+    });
+  }
+  else if(this._attr._grid._obj != this){
+    this._attr._grid.attach({
+      obj: this,
+      oxml: this.oxml
+    });
+  }
+}
+
+Editor.BuilderElement.prototype.detache_wnd = function detache_wnd() {
+  const {_grid} = this._attr;
+  if(_grid && _grid.destructor && _grid._owner_cell){
+    _grid._owner_cell.detachObject(true);
+    delete this._attr._grid;
+  }
+}
+
+
 class GlassInserts {
 
   constructor(glasses) {
