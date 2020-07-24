717389c9810a581f3239fb9567eb39dd3c1bc87d
unpete <info@oknosoft.ru>
2020-07-24 18:01:06 +0500

    унифицируем скрипт link

--- scripts/meta.link.js
+++ scripts/meta.link.js
@@ -6,17 +6,30 @@ const path = require('path');
 const fs = require('fs');
 const md5File = require('md5-file');
 
-const localWbModules = path.resolve(__dirname, '../node_modules/windowbuilder/dist');
-const remoteWbModules = 'D:\\WORK\\0KNOSOFT\\UniServer\\www\\builder2\\windowbuilder-core\\dist';
-const wbLibs = ['drawer.js', 'init.js'];
-
 const localNodeModules = path.resolve(__dirname, '../node_modules');
-const remoteNodeModules = 'D:\\WORK\\0KNOSOFT\\UniServer\\www\\builder2\\git-osde\\packages';
 const {dependencies} = require(path.resolve(__dirname, '../package.json'));
-const libs = Object.keys(dependencies).filter(v => /^metadata-/.test(v));
 
-const localForms = path.resolve(__dirname, '../node_modules/windowbuilder-forms/dist');
-const remoteForms  = 'D:\\WORK\\0KNOSOFT\\UniServer\\www\\builder2\\windowbuilder-forms\\dist';
+// накапливаем пути
+const repos = [
+  {
+    local: 'wb-core',
+    remote: 'D:\\WORK\\0KNOSOFT\\UniServer\\www\\builder2\\windowbuilder-core',
+    dir: 'dist',
+  },
+  {
+    local: 'wb-forms',
+    remote: 'D:\\WORK\\0KNOSOFT\\UniServer\\www\\builder2\\windowbuilder-forms',
+    dir: 'dist',
+  },
+];
+
+for(const local of Object.keys(dependencies).filter(v => /^metadata-/.test(v))) {
+  repos.push({
+    local,
+    remote: `D:\\WORK\\0KNOSOFT\\UniServer\\www\\builder2\\git-osde\\packages\\${local}`,
+    dir: '',
+  });
+}
 
 function fromDir(startPath, filter, callback) {
 
@@ -42,38 +55,11 @@ function fromDir(startPath, filter, callback) {
   };
 };
 
-let i = 0;
-for (const lib of wbLibs) {
-  const lname = path.resolve(localWbModules, lib);
-  const rname = path.resolve(remoteWbModules, lib);
-
-  if(!fs.existsSync(lname) || (md5File.sync(rname) != md5File.sync(lname))){
-    i++;
-    fs.createReadStream(rname).pipe(fs.createWriteStream(lname));
-  }
-}
-i && console.log(`from ${remoteWbModules} written ${i} files`);
-
-let j = 0;
-fromDir(remoteForms, /\.(css|js|mjs|md|map|gif|png)$/, (rname, isDir) => {
-  const name = rname.replace(remoteForms, '');
-  const lame = path.join(localForms, name);
-  if(isDir) {
-    if(!fs.existsSync(lame)) {
-      fs.mkdirSync(lame);
-    }
-  }
-  else if(!fs.existsSync(lame) || (md5File.sync(rname) != md5File.sync(lame))){
-    j++;
-    fs.createReadStream(rname).pipe(fs.createWriteStream(lame));
-  }
-});
-j && console.log(`from ${remoteForms} written ${j} files`);
-
+// исполняем
 let copied;
-for (const lib of libs) {
-  const lpath = path.resolve(localNodeModules, lib);
-  const rpath = path.resolve(remoteNodeModules, lib);
+for(const {local, remote, dir} of repos) {
+  const lpath = path.resolve(localNodeModules, local, dir);
+  const rpath = path.resolve(remote, dir);
   let i = 0;
   fromDir(rpath, /\.(css|js|mjs|md|map|gif|png)$/, (rname, isDir) => {
     const name = rname.replace(rpath, '');
@@ -93,6 +79,8 @@ for (const lib of libs) {
     console.log(`from ${rpath} written ${i} files`);
   }
 }
-if(!copied && !i && !j){
+
+
+if(!copied){
   console.log(`all files match`);
 }
