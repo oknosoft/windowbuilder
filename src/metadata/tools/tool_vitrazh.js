0aa228283620ad45e30da46c3bc768c19e17ac78
unpete <info@oknosoft.ru>
2021-06-30 12:01:48 +0300

    каркас инструмента 'Витраж'

--- src/metadata/tools/tool_vitrazh.js
+++ src/metadata/tools/tool_vitrazh.js
@@ -6,6 +6,7 @@
  * Created by Evgeniy Malyarov on 21.11.2020.
  */
 
+// http://localhost:3000/builder/b0b0fcf0-d593-11eb-8d4e-01b541f37852
 import ToolWnd from '../../components/Builder/ToolWnds/VitrazhWnd';
 
 export default function tool_vitrazh ({Editor, classes: {BaseDataObj}, dp: {builder_lay_impost}, cat: {characteristics}, utils, ui: {dialogs}}) {
@@ -29,9 +30,27 @@ export default function tool_vitrazh ({Editor, classes: {BaseDataObj}, dp: {buil
 
     on_activate() {
       super.on_activate('cursor-text-select');
-      const {options, project} = this;
-      this._scope.tb_left.select(options.name);
-      this._obj = builder_lay_impost.create();
+      const {options, project, _scope} = this;
+      _scope.tb_left && _scope.tb_left.select(options.name);
+      if(!project._attr._vitrazh) {
+        const obj = builder_lay_impost.create();
+        if(!project.contours.length) {
+          Editor.Contour.create({project});
+        }
+        const {bounds, l_dimensions} = project.contours[0];
+        obj.w = bounds.width;
+        obj.h = bounds.height;
+        for(const dl of l_dimensions.ihor.sizes()) {
+          obj.sizes.add({elm: 1, sz: dl.size});
+          obj.elm_by_y++;
+        }
+        for(const dl of l_dimensions.ivert.sizes()) {
+          obj.sizes.add({elm: 0, sz: dl.size});
+          obj.elm_by_x++;
+        }
+        project._attr._vitrazh = obj;
+      }
+      this._obj = project._attr._vitrazh;
     }
 
     on_deactivate() {
