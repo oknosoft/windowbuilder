2766f531669fdf2d11dfd5307455028fd54116f4
blackmixer <blackmixer666@yandex.ru>
2021-11-15 12:17:17 +0300

    toolbar with button info

--- src/components/Builder/Controls/ProductToolbar.js
+++ src/components/Builder/Controls/ProductToolbar.js
@@ -4,13 +4,19 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import Tip from 'wb-forms/dist/Common/Tip';
+import Btn from './Btn';
 import {useStyles} from './LayersToolbar';
 
 
 function ProductToolbar({project, classes}) {
   return <Toolbar disableGutters variant="dense">
     <Typography color="primary" className={classes.title}>Свойства изделия</Typography>
-    <Tip title="Обновить параметры">
+    <Tip title="кнопка">
+
+</Tip>
+        <Btn note={project._dp.sys.note}/>
+     
+<Tip title="Обновить параметры">
       <IconButton onClick={() => project._dp.sys.refill_prm(project.ox)}><i className="fa fa-retweet" /></IconButton>
     </Tip>
   </Toolbar>;
