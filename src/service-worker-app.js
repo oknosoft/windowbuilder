0d1d7ebb5daf439ed78f894c4a3b4ffa92bf9e34
unpete <info@oknosoft.ru>
2022-03-27 02:16:04 +0500

    мусор в swapp

--- src/service-worker-app.js
+++ src/service-worker-app.js
@@ -22,23 +22,6 @@ export default function () {
     //{url: '/styles/app.0c9a31.css', revision: null},
   ]);
 
-  const production = [], specification = [];
-  production.forEach(product => {
-    for (let i = 1; i <= product.quantity; i++) {
-      if (!product.characteristic.empty()) {
-        product.characteristic.specification.forEach(spec => {
-          specification.push(spec);
-        });
-      } else {
-        const prd = $p.utils._mixin({clr: $p.cat.clrs.get()}, product, ['nom','characteristic','price']);
-        prd.totqty1 = product.qty;
-        prd.price = product.first_cost;
-        specification.push(prd);
-      }
-    }
-    first_cost += product.first_cost * product.quantity;
-  });
-
 }
 
 
