c6a92598b1413ad47345532b5c5833af6878a4cf
unpete <info@oknosoft.ru>
2022-04-18 11:06:31 +0500

    сторона фурнитуры при определении штульповости профиля

--- src/service-worker-app.js
+++ src/service-worker-app.js
@@ -11,7 +11,7 @@ const dkey = new Date().toISOString().substr(0, 10);
 export default function () {
   skipWaiting();
 
-  const revision = '20220315';
+  const revision = '20220317';
 
   precacheAndRoute([
     {url: '/dist/dhtmlx.min.js', revision: null },
