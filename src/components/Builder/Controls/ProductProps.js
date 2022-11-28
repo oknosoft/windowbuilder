f9096a33505ac6a284f6f64d821f4a5561933f0c
unpete <info@oknosoft.ru>
2022-11-28 11:53:59 +0500

    версии core и forms

--- src/components/Builder/Controls/ProductProps.js
+++ src/components/Builder/Controls/ProductProps.js
@@ -3,6 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import PropField from 'metadata-react/DataField/PropField';
 import LinkedProps from 'wb-forms/dist/Common/LinkedProps';
+import FieldClr from 'wb-forms/dist/CatClrs/FieldClr';
 import RootToolbar from './Toolbar/RootToolbar';
 import ElmInsets from './ElmInsets';
 
@@ -11,15 +12,16 @@ export default function ProductProps(props) {
   const {project} = editor;
   const {_dp, constructor} = project;
   const elm = new constructor.FakePrmElm(project);
+  const cmeta = _dp._metadata('clr');
 
   // корректируем метаданные поля выбора цвета
-  const clr_group = $p.cat.clrs.selection_exclude_service(_dp._metadata('clr'), _dp, project);
+  const clr_group = $p.cat.clrs.selection_exclude_service(cmeta, _dp, project);
 
   return <>
     <RootToolbar project={project} ox={ox} _dp={_dp} />
     <PropField fullWidth _obj={_dp} _fld="sys"/>
-    <PropField fullWidth _obj={_dp} _fld="clr" clr_group={clr_group}/>
-    <LinkedProps ts={ox.params} cnstr={0} inset={elm.inset.ref}/>
+    <FieldClr fullWidth _obj={_dp} _fld="clr" _meta={cmeta} clr_group={clr_group}/>
+    <LinkedProps ts={ox.params} cnstr={0} inset={elm.inset.ref} project={project}/>
 
     <ElmInsets elm={elm}/>
   </>;
