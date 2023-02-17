6a098969987e8897f1c52accc21d0e79b95b739d
unpete <info@oknosoft.ru>
2023-02-17 21:10:38 +0500

    версия core

--- src/editor/element.js
+++ src/editor/element.js
@@ -11,50 +11,12 @@
  * Подключает окно редактор свойств текущего элемента, выбранного инструментом
  */
 Editor.BuilderElement.prototype.attache_wnd = function attache_wnd(cell) {
-  // if(!this._attr._grid || !this._attr._grid.cell){
-  //
-  //   this._attr._grid = cell.attachHeadFields({
-  //     obj: this,
-  //     oxml: this.oxml
-  //   });
-  //   this._attr._grid.attachEvent('onRowSelect', function (id) {
-  //     if(['x1', 'y1', 'a1', 'cnn1'].indexOf(id) != -1) {
-  //       this._obj.select_node('b');
-  //     }
-  //     else if(['x2', 'y2', 'a2', 'cnn2'].indexOf(id) != -1) {
-  //       this._obj.select_node('e');
-  //     }
-  //   });
-  // }
-  // else if(this._attr._grid._obj != this){
-  //   this._attr._grid.attach({
-  //     obj: this,
-  //     oxml: this.oxml
-  //   });
-  // }
-  // const ro = this.layer instanceof Editor.ContourNestedContent || this.layer instanceof Editor.ContourNested;
-  // const {buttons} = this.project._scope._acc.elm._otoolbar;
-  // this._attr._grid.setEditable(!ro);
-  // for(const btn in buttons) {
-  //   if(btn === 'spec') {
-  //     continue;
-  //   }
-  //   if(ro) {
-  //     buttons[btn].classList.add('disabledbutton');
-  //   }
-  //   else {
-  //     buttons[btn].classList.remove('disabledbutton');
-  //   }
-  // }
+
 };
 
 /**
  * Отключает и выгружает из памяти окно свойств элемента
  */
 Editor.BuilderElement.prototype.detache_wnd = function detache_wnd() {
-  // const {_grid} = this._attr;
-  // if(_grid && _grid.destructor && _grid._owner_cell){
-  //   _grid._owner_cell.detachObject(true);
-  //   delete this._attr._grid;
-  // }
+
 };
