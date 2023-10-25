00ef973bec69c2b8d4ce000a2ef32430abdb6b00
unpete <info@oknosoft.ru>
2023-10-25 21:15:25 +0300

    создание слоя ряда из интерфейса

--- src/components/Builder/ToolWnds/RegionLayer.js
+++ src/components/Builder/ToolWnds/RegionLayer.js
@@ -2,21 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import PropField from 'metadata-react/DataField/PropField';
 
-function RegionLayer({editor}) {
-  const {tool} = editor;
-  const {text} = tool;
-  return text ? <div>
-      <PropField _obj={text} _fld="text"/>
-      <PropField _obj={text} _fld="font_family"/>
-      <PropField _obj={text} _fld="bold" ctrl_type="cb"/>
-      <PropField _obj={text} _fld="font_size"/>
-      <PropField _obj={text} _fld="angle"/>
-      <PropField _obj={text} _fld="align" ctrl_type="oselect"/>
-      <PropField _obj={text} _fld="clr"/>
-      <PropField _obj={text} _fld="x"/>
-      <PropField _obj={text} _fld="y"/>
-    </div> :
-    <div>Элемент текста не выбран</div>;
+function RegionLayer({project, layer, region}) {
+  return <div>
+    {`layer.info #${region}`}
+    </div>;
 }
 
 RegionLayer.propTypes = {
@@ -25,20 +14,54 @@ RegionLayer.propTypes = {
 
 export function region_layer({Editor, ui: {dialogs}}) {
   dialogs.region_layer = function (project) {
-    const current = project.getActiveLayer();
-    if(!current || [0, 4].includes(current.kind)) {
+    const layer = project.getActiveLayer();
+    if(!layer || ![0, 4].includes(layer.kind)) {
       return dialogs.alert({
         title: 'Слой ряда',
         text: 'Текущий слой не выбран либо не может иметь рядов',
-        timeout: 180000,
+        timeout: 10000,
       });
     }
-    return dialogs.alert({
-      title: 'Слой ряда',
-      hide_btn: true,
-      timeout: 180000,
-      Component: RegionLayer
-    });
+    const insets = new Set;
+    const regions = new Set;
+    for(const {inset} of layer.profiles) {
+      for(const row of inset.inserts) {
+        if(row.inset.region) {
+          insets.add(row.inset);
+          regions.add(row.inset.region);
+        }
+      }
+    }
+    if(!insets.size) {
+      return dialogs.alert({
+        title: `Ряд для ${layer.info}`,
+        text: 'Вставки профилей текущего слоя не содержат рекомендуемых вставок рядов',
+        timeout: 10000,
+      });
+    }
+    const values = Array.from(regions).map(v => v.toFixed());
+    const initialValue = values[0];
+    let pre = Promise.resolve(initialValue);
+    if(regions.size > 1) {
+      pre = dialogs.input_value({
+        title: `Ряд для ${layer.info}`,
+        text: 'Уточните номер ряда',
+        list: values,
+        timeout: 10000,
+      });
+    }
+
+    return pre
+      .then((region) => {
+        return dialogs.alert({
+          title: `Ряд для ${layer.info}`,
+          hide_btn: true,
+          timeout: 180000,
+          Component: RegionLayer,
+          props: {project, layer, region}
+        });
+      })
+      .catch(() => null);
   };
 }
 export default RegionLayer;
--- src/metadata/common/index.js
+++ src/metadata/common/index.js
@@ -5,6 +5,7 @@ import i18ru from './i18n.ru';
 import scale_svg from './scale_svg';
 import tools from '../tools';
 import static_load from './static_load';
+import {region_layer} from '../../components/Builder/ToolWnds/RegionLayer';
 import templates_nested from 'wb-forms/dist/CalcOrder/TemplatesNested/dhtmlx';
 import {event_src} from 'metadata-react/common/proxy';
 import qs from 'qs';
@@ -13,6 +14,7 @@ export default function ($p) {
   i18ru($p);
   tools($p);
   static_load($p);
+  region_layer($p);
   templates_nested($p);
   event_src($p);
   $p.iface.scale_svg = scale_svg;
