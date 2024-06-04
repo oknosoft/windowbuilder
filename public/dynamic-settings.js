c4c12eeef2b51ce5279aa63a355087bf92cbf4e0
unpete <info@oknosoft.ru>
2024-06-04 08:44:15 +0500

    пропорциональный сдвиг

--- public/dynamic-settings.js
+++ public/dynamic-settings.js
@@ -2,16 +2,24 @@
  * Здесь определяем специфичные для хоста параметры
  */
 
-window._dynamic_patch_ = {
+(() => {
+  const keys21 = {
+    google: '',
+    yandex: '',
+    yasuggest: '',
+  };
 
-  predefined: {
-    zone: 10,
-    ram_indexer: false,
-  },
+  window._dynamic_patch_ = {
 
-  sentry: {
+    predefined: {
+      zone: 10,
+      keys: keys21,
+      ram_indexer: false,
+    },
 
-  }
-};
+  };
+
+})();
+
 
 // при желании-необходимости, здесь можно разместить...
