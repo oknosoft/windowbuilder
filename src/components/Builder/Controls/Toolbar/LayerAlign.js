56586fc82259ec93c63711483499ba1aed1d87d6
unpete <info@oknosoft.ru>
2025-11-25 14:17:43 +0500

    приклеивание слоёв

--- src/components/Builder/Controls/Toolbar/LayerAlign.js
+++ src/components/Builder/Controls/Toolbar/LayerAlign.js
@@ -3,20 +3,66 @@ import Tip from 'metadata-react/App/Tip';
 import IconButton from '@material-ui/core/IconButton';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import Badge from '@material-ui/core/Badge';
+import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
+import SettingsBackupRestoreIcon from '@material-ui/icons/SettingsBackupRestore';
 import VerticalAlignCenterIcon from '@material-ui/icons/VerticalAlignCenter';
+import SubdirectoryArrowLeftIcon from '@material-ui/icons/SubdirectoryArrowLeft';
+import SubdirectoryArrowRightIcon from '@material-ui/icons/SubdirectoryArrowRight';
+
+function execJoin(smap, handleClose, editor, dir) {
+  handleClose();
+  const {order} = smap;
+  const l1 = smap.get(order[0]);
+  const l2 = smap.get(order[1]);
+  switch (dir) {
+    case 'rt':
+      l2.move([l1.bounds.right - l2.bounds.left, l1.bounds.top - l2.bounds.top]);
+      break;
+    case 'rb':
+      l2.move([l1.bounds.right - l2.bounds.left, l1.bounds.bottom - l2.bounds.bottom]);
+      break;
+    case 'br':
+      l2.move([l1.bounds.right - l2.bounds.right, l1.bounds.bottom - l2.bounds.top]);
+      break;
+    case 'bl':
+      l2.move([l1.bounds.left - l2.bounds.left, l1.bounds.bottom - l2.bounds.top]);
+      break;
+    case 'lt':
+      l2.move([l1.bounds.left - l2.bounds.right, l1.bounds.top - l2.bounds.top]);
+      break;
+    case 'lb':
+      l2.move([l1.bounds.left - l2.bounds.right, l1.bounds.bottom - l2.bounds.bottom]);
+      break;
+    case 'tr':
+      l2.move([l1.bounds.right - l2.bounds.right, l1.bounds.top - l2.bounds.bottom]);
+      break;
+    case 'tl':
+      l2.move([l1.bounds.left - l2.bounds.left, l1.bounds.top - l2.bounds.bottom]);
+      break;
+  }
+  editor.eve.once('coordinates_calculated', (project) => {
+    project.zoom_fit();
+    project.draw_selection();
+  });
+}
 
 export default function LayerAlign({editor}) {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const handleClose = () => setAnchorEl(null);
-  const {consts} = editor;
+  const {consts, project} = editor;
+  const {smap} = project._attr;
   const [selectMode, setMode] = React.useState(consts.mode === 'select');
   const handleMode = () => {
     consts.mode = selectMode ? '' : 'select';
     setMode(!selectMode);
-    editor.project.draw_selection();
+    project.draw_selection();
     handleClose();
   };
+  const disabled = !smap || smap.size < 2;
+  const join = (dir) => execJoin(smap, handleClose, editor, dir);
 
   return <>
     <Tip title={'Выравнивание слоёв' + (selectMode ? ' включено' : '')}>
@@ -33,9 +79,63 @@ export default function LayerAlign({editor}) {
       open={Boolean(anchorEl)}
       onClose={handleClose}
     >
-      <MenuItem onClick={handleMode}>{selectMode ? 'Перейти в стандартный режим' : 'Активировать'}</MenuItem>
-      <MenuItem onClick={handleClose}>My account</MenuItem>
-      <MenuItem onClick={handleClose}>Logout</MenuItem>
+      <MenuItem onClick={handleMode}>
+        <ListItemIcon>
+          {selectMode ? <SettingsBackupRestoreIcon fontSize="small"/> : <CheckBoxOutlineBlankIcon fontSize="small"/>}
+        </ListItemIcon>
+        <ListItemText primary={selectMode ? 'Перейти в стандартный режим' : 'Активировать'} />
+      </MenuItem>
+      <MenuItem disabled={disabled} onClick={() => join('rt')}>
+        <ListItemIcon>
+          <SubdirectoryArrowRightIcon fontSize="small" style={{transform: 'rotate(0.5turn)'}}/>
+        </ListItemIcon>
+        <ListItemText primary="Справа вверх" />
+      </MenuItem>
+      <MenuItem disabled={disabled} onClick={() => join('rb')}>
+        <ListItemIcon>
+          <SubdirectoryArrowLeftIcon fontSize="small" />
+        </ListItemIcon>
+        <ListItemText primary="Справа вниз" />
+      </MenuItem>
+
+      <MenuItem disabled={disabled} onClick={() => join('br')}>
+        <ListItemIcon>
+          <SubdirectoryArrowRightIcon fontSize="small" style={{transform: 'rotate(0.75turn)'}}/>
+        </ListItemIcon>
+        <ListItemText primary="Снизу вправо" />
+      </MenuItem>
+      <MenuItem disabled={disabled} onClick={() => join('bl')}>
+        <ListItemIcon>
+          <SubdirectoryArrowLeftIcon fontSize="small" style={{transform: 'rotate(0.25turn)'}}/>
+        </ListItemIcon>
+        <ListItemText primary="Снизу влево" />
+      </MenuItem>
+
+      <MenuItem disabled={disabled} onClick={() => join('lt')}>
+        <ListItemIcon>
+          <SubdirectoryArrowLeftIcon fontSize="small" style={{transform: 'rotate(0.5turn)'}}/>
+        </ListItemIcon>
+        <ListItemText primary="Слева вверх" />
+      </MenuItem>
+      <MenuItem disabled={disabled} onClick={() => join('lb')}>
+        <ListItemIcon>
+          <SubdirectoryArrowRightIcon fontSize="small"/>
+        </ListItemIcon>
+        <ListItemText primary="Слева вниз" />
+      </MenuItem>
+
+      <MenuItem disabled={disabled} onClick={() => join('tr')}>
+        <ListItemIcon>
+          <SubdirectoryArrowLeftIcon fontSize="small" style={{transform: 'rotate(0.75turn)'}}/>
+        </ListItemIcon>
+        <ListItemText primary="Сверху вправо" />
+      </MenuItem>
+      <MenuItem disabled={disabled} onClick={() => join('tl')}>
+        <ListItemIcon>
+          <SubdirectoryArrowRightIcon fontSize="small" style={{transform: 'rotate(0.25turn)'}}/>
+        </ListItemIcon>
+        <ListItemText primary="Сверху влево" />
+      </MenuItem>
     </Menu>
   </>;
 }
